24b643c6bbd4334a1251263e12814cb74a20b637
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-08-04 16:50:32 -0300

    Repositories

---
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -0,0 +1,17 @@
+package com.devsuperior.movieflix.repositories;
+
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.entities.Movie;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    @Query ("SELECT  DISTINCT m FROM Movie m " +
+            "INNER JOIN m.genre g " +
+            "WHERE (:genre IS NULL OR :genre IN g ) ORDER BY m.title " )
+    Page<Movie> findMovieByGenre(Genre genre, Pageable pageable);
+
+}
