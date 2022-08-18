7826c2860e9f7c6e0e0e221e3d67aa08d503afcd
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-08-18 18:02:34 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
     <>
-    <p>Vanessa</p>
+      <Navbar />
     </>
   );
 }
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,16 @@
+import './styles.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-primary main-nav">
+      <div className="container-fluid">
+        <a href="link" className="nav-logo-text">
+          <h4>MovieFlix</h4>
+        </a>
+        <div></div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
