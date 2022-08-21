47316f5305214126f8dea9e12e86ccfdf341be43
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-08-21 16:01:35 -0300

    Configuração básica de validação de formulário com react-hook-form

--- frontend/src/pages/Home/Login/index.tsx
+++ frontend/src/pages/Home/Login/index.tsx
@@ -1,45 +1,58 @@
 import Button from 'components/Button';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { requestBackendLogin } from 'util/requests';
 import './styles.css';
 
-
 type FormData = {
   username: string;
   password: string;
-}
+};
 const Login = () => {
+  const [hasError, setHasError] = useState(false);
 
-  const { register, handleSubmit} = useForm<FormData>();
+  const { register, handleSubmit, formState: {errors} } = useForm<FormData>();
 
-  const onSubmit = (formData : FormData) => {
+  const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
-    .then(response => {
-
-      console.log('SUCESSO', response);
-    })
-    .catch(error => {
-      console.log('ERRO', error);
-    });
+      .then((response) => {
+        setHasError(false);
+        console.log('SUCESSO', response);
+      })
+      .catch((error) => {
+        setHasError(true);
+        console.log('ERRO', error);
+      });
   };
 
   return (
     <div className="home-login">
       <h1>login</h1>
+      {hasError && (
+        <div className="alert alert-danger">Preencha os dados corretamente</div>
+      )}
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input 
-        {...register("username")}
-        type="text" 
-        placeholder="Email"
-        name="username" 
+        <input
+          {...register('username', {
+            required: 'Campo obrigatório'
+          })}
+          type="text"
+          placeholder="Email"
+          name="username"
         />
-        <input 
-        {...register("password")}
-        type="password" 
-        placeholder="Senha" 
-        name="password"
-        />
-      <Button text="fazer login"/>
+        {/* Pode ser colocada a classe mb-2 do bootstrap para facilitar a responsividade para isso é preciso envolver o input e a div abaixo com uma outro div que receberá a classe mb-2 */}
+        <div className="invalid-feedback d-block">{errors.username?.message}</div>
+        <input
+          {...register('password', {
+            required: 'Campo obrigatório'
+          })}
+          type="password"
+          placeholder="Senha"
+          name="password"
+        /> 
+        {/* Pode ser colocada a classe mb-2 do bootstrap para facilitar a responsividade para isso é preciso envolver o input e a div abaixo com uma outro div que receberá a classe mb-2 */}
+        <div className="invalid-feedback d-block">{errors.password?.message}</div>
+        <Button text="fazer login" />
       </form>
     </div>
   );
