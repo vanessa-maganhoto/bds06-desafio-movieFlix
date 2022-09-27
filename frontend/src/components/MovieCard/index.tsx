573eef935652f4541e32c82ad8c1749c988df46c
vanessa-maganhoto <vanessammaganhoto@gmail.com>
2022-09-27 15:50:19 -0300

    Estilização do MovieCard da página MovieCatalog

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -6,15 +6,15 @@ type Props = {
   movie: Movie;
 };
 const MovieCard = ({ movie }: Props) => {
-  
   return (
-    <div>
-        
-      <div>
-          <img src={movie.imgUrl} alt={movie.title} />
-          <h2>{movie.title}</h2>
-          <h3>{movie.year}</h3>
-          <p>{movie.subTitle}</p>
+    <div className="movie-card-container">
+      <div className="movie-card-img">
+        <img src={movie.imgUrl} alt={movie.title} />
+      </div>
+      <div className="movie-card-info">
+        <h2>{movie.title}</h2>
+        <h3>{movie.year}</h3>
+        <p>{movie.subTitle}</p>
       </div>
     </div>
   );
